f0b7a38432371b6aea2a9ff084c57ca7dec22204
Karna Suara <72575775+sskrn@users.noreply.github.com>
2026-09-16 21:17:46 +0700

    Delete version.js

--- version.js
+++
@@ -1,5 +0,0 @@
-{
-  "version": "1.5.5",
-  "url": "https://github.com/sskrn/dl01-releases/releases/download/v1.5.5/DL01_RS232_WiFi.bin",
-  "notes": "v1.5.5: Factory SN system, Auto-IMEI detection, stability fixes."
-}
